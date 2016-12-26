143a8e06ad8230f448786e169c53d11c8240b060
Martin Kim Dung-Pham <kim@elbedev.com>
2016-12-26 11:36:20 +0100

    require date header to be set on request

--- requesthandling/requestmanager_test.go
+++ requesthandling/requestmanager_test.go
@@ -1,18 +1,28 @@
 package requesthandling
 
 import (
-  "testing"
+	"testing"
+	"unicode/utf8"
 )
 
-func TestRequest(t *testing.T) {
-  requestManager := RequestManager{}
-  request, err := requestManager.PingRequest()
+func TestPingRequest(t *testing.T) {
+	requestManager := RequestManager{}
+	request, err := requestManager.PingRequest()
 
-  if request == nil {
-    t.Errorf("The Ping Request must not be nil")
-  }
+	if request == nil {
+		t.Errorf("The Ping Request must not be nil")
+	}
 
-  if err != nil {
-    t.Errorf("A Ping Request must not result in error")
-  }
+	if err != nil {
+		t.Errorf("A Ping Request must not result in error")
+	}
+}
+
+func TestPingRequestDateParameter(t *testing.T) {
+	requestManager := RequestManager{}
+	request, _ := requestManager.PingRequest()
+	dateString := request.Header.Get("X-Apple-CloudKit-Request-ISO8601Date")
+	if utf8.RuneCountInString(dateString) <= 0 {
+		t.Errorf("The Ping Request's date header is required to be set")
+	}
 }
